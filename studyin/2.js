4ea644bd2025e6205168187b65993fdc0f74060a
mvfasatresor <mvfasatresor@gmail.com>
2024-12-13 13:06:52 +0200

    ...

---
+++ studyin/2.js
@@ -0,0 +1,8 @@
+let age = 10;
+if (age < 13) {
+    console.log("You are a child.");
+} else if (age >= 13 && age <= 19) {
+    console.log("You are a teenager.");
+} else if (age >= 20) {
+    console.log("You are an adult.");
+}
